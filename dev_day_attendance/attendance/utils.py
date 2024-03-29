73503042790c8caae2c5521bbaee1be30d15d4c4
Munnazzar <munnzzar0214@gmail.com>
2024-03-29 09:51:20 +0500

    Update utils.py

--- dev_day_attendance/attendance/utils.py
+++ dev_day_attendance/attendance/utils.py
@@ -3,9 +3,9 @@ from pyproj import Geod
 
 
 def WGS84_Distance_Calc(fastLat, fastLng, usrLat, usrLng):
-    print("coordinates: ", fastLat, fastLng, usrLat, usrLng)
+    #print("coordinates: ", fastLat, fastLng, usrLat, usrLng)   #debugging purposes
     geod = Geod(ellps='WGS84')
-    dist = geod.inv(fastLng, fastLat, usrLng, usrLat)[2]
-    print("distance: ", dist)
-    return dist
+    dist = geod.inv(fastLng, fastLat, usrLng, usrLat)[2]        
+    #print("distance: ", dist)
+    return abs(dist)
 
